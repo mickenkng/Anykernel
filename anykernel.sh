19d30705b37908bb00bf27c9ae164efa5c4a1af2
mickenkng <mickenkng@yahoo.com>
2021-05-30 18:48:14 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=VantomBE by Vantom & Karou
+kernel.string=XimiKernul by MickenKng
 do.devicecheck=1
 do.modules=0
 do.systemless=1
